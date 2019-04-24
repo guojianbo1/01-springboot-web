1a16aa1c744a560ec18590644413e6aee8a9f81e
jianbo <840721178@qq.com>
2019-04-24 16:02:59 +0800

    01test

---
+++ src/main/java/com/myfirst/springboot/controller/HelloController.java
@@ -0,0 +1,19 @@
+package com.myfirst.springboot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * @Auther: GJB
+ * @Date: 2019/4/24 14:08
+ * @Description:
+ */
+@Controller
+public class HelloController {
+
+    @RequestMapping("/boot/hello")
+    public @ResponseBody String sayhello(){
+        return "Hello SpringBoot";
+    }
+}
