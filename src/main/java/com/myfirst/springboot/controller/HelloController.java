7d6870a24b1417cf9bf705523e87f566e3663091
jianbo <840721178@qq.com>
2019-04-25 14:49:59 +0800

    01test5

--- src/main/java/com/myfirst/springboot/controller/HelloController.java
+++ src/main/java/com/myfirst/springboot/controller/HelloController.java
@@ -14,6 +14,6 @@ public class HelloController {
 
     @RequestMapping("/boot/hello")
     public @ResponseBody String sayhello(){
-        return "Hello SpringBoot4";
+        return "Hello SpringBoot5";
     }
 }
